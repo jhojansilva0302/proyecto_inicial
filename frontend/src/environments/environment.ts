a6f1c18a948ce272038a9448f8accbbdb20701d2
Jhojan Silva <jhojansilva0302@gmail.com>
2026-04-09 17:30:14 -0500

    Url de produccion configurada

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000' // TODO: Cambiar por la URL de producción
+  apiUrl: 'https://proyectoinicial-production.up.railway.app'
 };
